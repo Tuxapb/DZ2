5bfcb775323032525dc29b2c5c3febdc1e42d686
Uzhvenko Andrey <uzhvenko@mail.ru>
2023-02-15 20:59:25 +0300

    сохраняем

---
+++ 2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру
+//  заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+switch (number1)
+{
+    case  int n when (n < 100):
+        Console.Write($"Третьей цифры нет");
+        break;
+    case  int n when (n > 100 && n <1000):
+        Console.Write($"Третья цифра = {n%10}");
+        break;
+    case  int n when (n > 1000 && n <10000):
+        Console.Write($"Третья цифра = {n=(n/10)%10}");
+        break;
+    case  int n when (n > 10000 && n <100000):
+        Console.Write($"Третья цифра = {n=(n/100)%10}");
+        break;    
+}
---
+++ 3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 15: Напишите программу, 
+// которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        Console.Write("нет");
+        break;
+
+    case 2:
+        Console.Write("Нет");
+        break;
+
+    case 3:
+        Console.Write("Нет");
+        break;
+
+    case 4:
+        Console.Write("Нет");
+        break;
+
+    case 5:
+        Console.Write("Нет");
+        break;
+
+    case 6:
+        Console.Write("Да");
+        break;
+
+    case 7:
+        Console.Write("Да");
+        break;
+
+    default:
+        Console.Write("Не верное значение!");
+        break;
+}
+
